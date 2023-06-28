52b6ba6cc4f7ee8a6cf34c25f335134a63017ed3
Khoa Nguyen <willykhoa.nguyen@gmail.com>
2023-06-28 21:21:42 +0200

    Udemy - R Programming - Section 3 - Homework - Financial Statement analysis

--- code.R
+++ code.R
@@ -34,3 +34,37 @@ for (i in rnorm(N)) {
 }
 answer <- counter / N
 answer
+
+#financial statement analysis
+#data set available at superdatascience.com/rcourse
+
+#Data
+revenue <- c(14574.49, 7606.46, 8611.41, 9175.41, 8058.65, 8105.44, 11496.28, 9766.09, 10305.32, 14379.96, 10713.97, 15433.50)
+expenses <- c(12051.82, 5695.07, 12319.20, 12089.72, 8658.57, 840.20, 3285.73, 5821.12, 6976.93, 16618.61, 10054.37, 3803.96)
+
+#Solution
+
+mprop <- revenue - expenses
+
+profit_at<- mprop-mprop*0.3
+
+margin <- profit_at/revenue*100
+
+index <- profit_at - mean(profit_at)
+
+good_month <- which(index >=0)
+
+bad_month <- which(index <0)
+
+best_month <- which.max(profit_at)
+
+worst_month <- which.min(profit_at)
+
+#Results
+round(mprop)
+round(profit_at)
+round(margin)
+good_month
+bad_month
+best_month
+worst_month
